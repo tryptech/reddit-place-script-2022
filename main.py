57dd722dfb090e28908a05b6cd081f330b231683
marckopop <marckos@popsells.com>
2023-07-21 13:41:48 -0400

    Add origin and referer to login post request

--- main.py
+++ main.py
@@ -556,7 +556,9 @@ class PlaceClient:
                             client.proxies = proxy.get_random_proxy(self, name)
                             client.headers.update(
                                 {
-                                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36"
+                                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+                                    "Origin": "https://www.reddit.com",
+                                    "Referer": "https://www.reddit.com/login/?",
                                 }
                             )
 
