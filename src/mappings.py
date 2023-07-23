d8b2fcbebfdd5f2de23534faff43f95f7529a4bd
tryptech <654389+tryptech@users.noreply.github.com>
2023-07-23 14:23:17 -0400

    correct colors

--- src/mappings.py
+++ src/mappings.py
@@ -4,35 +4,35 @@ from PIL import ImageColor
 
 class ColorMapper:
     COLOR_MAP = {
-        "#6D001A": 0,  # darkest red
+        #"#6D001A": 0,  # darkest red
         "#BE0039": 1,  # dark red
         "#FF4500": 2,  # red
         "#FFA800": 3,  # orange
         "#FFD635": 4,  # yellow
-        "#FFF8B8": 5,  # pale yellow
+        #"#FFF8B8": 5,  # pale yellow
         "#00A368": 6,  # dark green
         "#00CC78": 7,  # green
         "#7EED56": 8,  # light green
         "#00756F": 9,  # dark teal
         "#009EAA": 10,  # teal
-        "#00CCC0": 11,  # light teal
+        #"#00CCC0": 11,  # light teal
         "#2450A4": 12,  # dark blue
         "#3690EA": 13,  # blue
         "#51E9F4": 14,  # light blue
         "#493AC1": 15,  # indigo
         "#6A5CFF": 16,  # periwinkle
-        "#94B3FF": 17,  # lavender
+        #"#94B3FF": 17,  # lavender
         "#811E9F": 18,  # dark purple
-        "#B44AC0": 19,  # purple
-        "#E4ABFF": 20,  # pale purple
-        "#DE107F": 21,  # magenta
+        #"#B44AC0": 19,  # purple
+        #"#E4ABFF": 20,  # pale purple
+        #"#DE107F": 21,  # magenta
         "#FF3881": 22,  # pink
         "#FF99AA": 23,  # light pink
         "#6D482F": 24,  # dark brown
         "#9C6926": 25,  # brown
-        "#FFB470": 26,  # beige
+        #"#FFB470": 26,  # beige
         "#000000": 27,  # black
-        "#515252": 28,  # dark gray
+        #"#515252": 28,  # dark gray
         "#898D90": 29,  # gray
         "#D4D7D9": 30,  # light gray
         "#FFFFFF": 31,  # white
